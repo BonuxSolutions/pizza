5d23acc6a0649f64a90f24e332f098b8b9a33d63
Andis Krauklis <a.krauklis@intrum.com>
2018-02-16 16:20:54 +0200

    reading from file

--- src/main/java/pizza/FileUtils.java
+++ src/main/java/pizza/FileUtils.java
@@ -1,56 +1,67 @@
 package pizza;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static pizza.FunctionsAndConstants.M;
 import static pizza.FunctionsAndConstants.T;
 
 abstract class FileUtils {
-  static Pizza readInput(String filename) {
-    try (FileInputStream fis = new FileInputStream(new File(filename))) {
-      String contents = new String(fis.readAllBytes());
-      String[] lines = contents.split("\n");
+    static Pizza readInput(String filename) {
 
-      String[] firstLine = lines[0].split(" ");
+        StringBuilder contentBuilder = new StringBuilder();
 
-      if (firstLine.length != 4) {
-        throw new IllegalArgumentException(
-            "not enough parameters in first line:" + Arrays.toString(firstLine));
-      }
-      int R = Integer.parseInt(firstLine[0]);
-      int C = Integer.parseInt(firstLine[1]);
-
-      if (lines.length - 1 != R) {
-        throw new IllegalArgumentException(
-            String.format("number %d of rows do not match %d", lines.length, R)
-        );
-      }
-
-      int L = Integer.parseInt(firstLine[2]);
-      int H = Integer.parseInt(firstLine[3]);
-
-      int[][] toppings = new int[R][C];
-
-      for (int r = 1; r < R; r++) {
-        char[] line = lines[r].toCharArray();
-        for (int c = 0; c < C; c++) {
-          toppings[r - 1][c] = ('T' == line[c]) ? T : M;
+        try (Stream<String> stream = Files.lines(Paths.get(filename), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-      }
 
-      return new Pizza.Builder()
-          .withRows(R)
-          .withCols(C)
-          .withMinIngredient(L)
-          .withTotalCells(H)
-          .withToppings(toppings)
-          .build();
-    } catch (IOException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+        try {
+
+            String[] lines = contentBuilder.toString().split("\n");
+            String[] firstLine = lines[0].split(" ");
+
+            if (firstLine.length != 4) {
+                throw new IllegalArgumentException(
+                        "not enough parameters in first line:" + Arrays.toString(firstLine));
+            }
+            int R = Integer.parseInt(firstLine[0]);
+            int C = Integer.parseInt(firstLine[1]);
+
+            if (lines.length - 1 != R) {
+                throw new IllegalArgumentException(
+                        String.format("number %d of rows do not match %d", lines.length, R)
+                );
+            }
+
+            int L = Integer.parseInt(firstLine[2]);
+            int H = Integer.parseInt(firstLine[3]);
+
+            int[][] toppings = new int[R][C];
+
+            for (int r = 1; r < R; r++) {
+                char[] line = lines[r].toCharArray();
+                for (int c = 0; c < C; c++) {
+                    toppings[r - 1][c] = ('T' == line[c]) ? T : M;
+                }
+            }
+
+            return new Pizza.Builder()
+                    .withRows(R)
+                    .withCols(C)
+                    .withMinIngredient(L)
+                    .withTotalCells(H)
+                    .withToppings(toppings)
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
