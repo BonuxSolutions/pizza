9d0eb58f52e077414370345cf66992ddb45a60ca
Renars Kudins <renars.kudins@gmail.com>
2018-02-20 12:59:39 +0200

    divisors, slices and jshell

--- src/main/java/pizza/App.java
+++ src/main/java/pizza/App.java
@@ -1,11 +1,16 @@
 package pizza;
 
+import static java.lang.System.out;
+
 public class App {
   public String getGreeting() {
     return "Hello world.";
   }
 
   public static void main(String[] args) {
-    System.out.println(new App().getGreeting());
+    out.println(new App().getGreeting());
+
+    FunctionsAndConstants.possibleSlices(5)
+        .forEach(out::println);
   }
 }
--- src/main/java/pizza/Slices.java
+++ src/main/java/pizza/Slices.java
@@ -6,87 +6,92 @@ import java.util.Objects;
 import java.util.Set;
 
 final class Slice {
-    final int r1, c1, r2, c2;
+  final int r1, c1, r2, c2;
 
-    private Slice(int r1, int c1, int r2, int c2) {
-        this.r1 = r1;
-        this.c1 = c1;
-        this.r2 = r2;
-        this.c2 = c2;
+  private Slice(int r1, int c1, int r2, int c2) {
+    this.r1 = r1;
+    this.c1 = c1;
+    this.r2 = r2;
+    this.c2 = c2;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Slice slice = (Slice) o;
+    return r1 == slice.r1 &&
+        c1 == slice.c1 &&
+        r2 == slice.r2 &&
+        c2 == slice.c2;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(r1, c1, r2, c2);
+  }
+
+  static class Builder {
+    private int r1, c1, r2, c2;
+
+    Builder withUpperLeft(int r1, int c1) {
+      this.r1 = r1;
+      this.c1 = c1;
+      return this;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Slice slice = (Slice) o;
-        return r1 == slice.r1 &&
-                c1 == slice.c1 &&
-                r2 == slice.r2 &&
-                c2 == slice.c2;
+    Builder withLowerRight(int r2, int c2) {
+      this.r2 = r2;
+      this.c2 = c2;
+      return this;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(r1, c1, r2, c2);
+    Slice build() {
+      return new Slice(r1, c1, r2, c2);
     }
+  }
 
-    static class Builder {
-        private int r1, c1, r2, c2;
-
-        Builder withUpperLeft(int r1, int c1) {
-            this.r1 = r1;
-            this.c1 = c1;
-            return this;
-        }
-
-        Builder withLowerRight(int r2, int c2) {
-            this.r2 = r2;
-            this.c2 = c2;
-            return this;
-        }
-
-        Slice build() {
-            return new Slice(r1, c1, r2, c2);
-        }
-    }
+  @Override
+  public String toString() {
+    return "(" + r1 + "," + c1 + ") (" + r2 + "," + c2 + ")";
+  }
 }
 
 final class SliceBase {
-    final int r, c;
+  final int r, c;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof SliceBase) {
-            SliceBase v = (SliceBase) obj;
-            return v.c == this.c && v.r == this.r;
-        } else {
-            return false;
-        }
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof SliceBase) {
+      SliceBase v = (SliceBase) obj;
+      return v.c == this.c && v.r == this.r;
+    } else {
+      return false;
     }
+  }
 
-    private SliceBase(int r, int c) {
-        this.r = r;
-        this.c = c;
-    }
+  private SliceBase(int r, int c) {
+    this.r = r;
+    this.c = c;
+  }
 
-    static SliceBase create(int r, int c) {
-        return new SliceBase(r, c);
-    }
+  static SliceBase create(int r, int c) {
+    return new SliceBase(r, c);
+  }
 }
 
 final class SlicesPerBase {
-    final Set<Slice> slices;
+  final Set<Slice> slices;
 
-    void addSlices(final List<Slice> slices) {
-        this.slices.addAll(slices);
-    }
+  void addSlices(final List<Slice> slices) {
+    this.slices.addAll(slices);
+  }
 
-    private SlicesPerBase(int r, int c) {
-        slices = new HashSet<>(c * r);
-    }
+  private SlicesPerBase(int r, int c) {
+    slices = new HashSet<>(c * r);
+  }
 
-    static SlicesPerBase create(int r, int c) {
-        return new SlicesPerBase(r, c);
-    }
+  static SlicesPerBase create(int r, int c) {
+    return new SlicesPerBase(r, c);
+  }
 }
