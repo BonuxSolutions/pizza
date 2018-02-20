ac727eee2a626ecd2738e8d87ffc07368eef6420
Renars Kudins <renars.kudins@gmail.com>
2018-02-20 23:16:15 +0200

    back to pre-historic java

--- jshell.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-JAVA_HOME=$1 \
-JAVA_OPTS="--add-exports jdk.jshell/jdk.internal.jshell.tool=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED" \
-./gradlew --no-daemon --console plain jshell
