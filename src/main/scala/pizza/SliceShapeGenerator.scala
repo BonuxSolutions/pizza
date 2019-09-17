6fe9b6ddbd929b012a1865827f530069bd3de285
Renars Kudins <renars.kudins@gmail.com>
2019-09-17 16:39:11 +0300

    added shape generator

---
+++ src/main/scala/pizza/SliceShapeGenerator.scala
@@ -0,0 +1,3 @@
+package pizza
+
+object SliceShapeGenerator
--- src/main/scala/pizza/SlicerStrategy.scala
+++ src/main/scala/pizza/SlicerStrategy.scala
@@ -3,12 +3,17 @@ package pizza
 trait SlicerStrategy {
   def startX: Int
   def startY: Int
+  def sliceSize: SliceSize
 }
 
+sealed trait SliceSize
 object SlicerStrategy {
+  case object MaxSlice extends SliceSize
+  case object MinSlice extends SliceSize
 
-  case object UpperLeft extends SlicerStrategy {
+  case object UpperLeftMaxSize extends SlicerStrategy {
     override val startX = 0
     override val startY = 0
+    override val sliceSize = MaxSlice
   }
 }
